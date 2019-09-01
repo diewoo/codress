80fba9a9ed42bce4e404fe63df4af10741f2a95e
Diego <diego@kambista.com>
2019-08-31 21:53:50 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
 ReactDOM.render(
-    <BrowserRouter>
+    <Provider store={store}>
+   <BrowserRouter>
         <App />
     </BrowserRouter>
-    , document.getElementById('root'));
+     </Provider>,
+    document.getElementById('root'));
